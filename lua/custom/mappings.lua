61329b5ea9267d79d507b193df4eb2620933435c
Jimba Tshetrim <jtshetrim@gmail.com>
2024-03-19 00:31:24 +0600

    feat: git keybinding

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -4,7 +4,11 @@ local M = {}
 M.general = {
   n = {
     -- [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ['<M-O>'] = {":OrganizeImports <CR>", "OrganizeImports"}
+    ['<M-O>'] = {":OrganizeImports <CR>", "OrganizeImports"},
+    ['<S-M-Down>'] = {":Git next_hunk <CR> :Git preview_hunk_inline <CR>", "Git next hunk"},
+    ['<S-M-Up>'] = {":Git prev_hunk <CR> :Git preview_hunk_inline <CR>", "Git previous hunk"},
+    ['<S-M-z>'] = {":Git reset_hunk <CR>", "Git reset hunk"},
+    ['<M-_>'] = {":Git preview_hunk <CR>", "Git preview hunk"},
   },
   v = {
     [">"] = { ">gv", "indent"},
